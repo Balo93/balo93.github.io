a537a018c11e99f52e005d5664fbb60f8a2b0680
Balo93 <Balo93@users.noreply.github.com>
2025-12-30 10:12:08 +0000

    deploy: cdffd41d061036481775361f4714c67849e00c6a

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767075068',resources: [
+    cacheName: 'chirpy-1767089527',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
